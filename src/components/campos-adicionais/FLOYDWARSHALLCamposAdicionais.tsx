d22ed759ac212270b7f94e914392d3cbbf7934c7
NicolasZanatto <34211411+NicolasZanatto@users.noreply.github.com>
2021-10-13 20:47:43 -0300

    Update FLOYDWARSHALLCamposAdicionais.tsx

--- src/components/campos-adicionais/FLOYDWARSHALLCamposAdicionais.tsx
+++ src/components/campos-adicionais/FLOYDWARSHALLCamposAdicionais.tsx
@@ -9,9 +9,9 @@ const FLOYDWARSHALLCamposAdicionais = (props: Props) => {
     const passo = props.simulacao.floydWarshall.caminho[props.simulacao.passo];
     const itensCamposAdicionais: ICamposAdicionais = {
         itens: [
+            { id: "vertice-k", titulo: "vertice k", valor: passo?.verticeK?.toString() },
             { id: "vertice-i", titulo: "Vértice i", valor: passo?.verticeI?.toString() },
             { id: "vertice-j", titulo: "vertice j", valor: passo?.verticeJ?.toString() },
-            { id: "vertice-k", titulo: "vertice k", valor: passo?.verticeK?.toString() },
             { id: "distancia-i-j", titulo: "distância (i,j)", valor: passo?.distanciaIJ?.toString() },
             { id: "distancia-i-k", titulo: "distância (i,k)", valor: passo?.distanciaIK?.toString() },
             { id: "distancia-k-j", titulo: "distância (k,j)", valor: passo?.distanciaKJ?.toString() },
